c619697aa412c1007caefb09bce59a63c4466649
nursinghamza-hash <nursinghamza@gmail.com>
2026-08-26 03:54:55 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pesarena.gaza"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
